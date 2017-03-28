bb215cd00e1fbaa52f15e85ab4a1d1cec1e56993
rcasalla <rcasalla@uniandes.edu.co>
2017-03-27 21:41:55 -0500

    Correccion paso 4 borrar funcion repetida Signed-off-by:rcasalla <rcasalla@uniandes.edu.co>

--- src/main/webapp/src/modules/books/books.mod.js
+++ src/main/webapp/src/modules/books/books.mod.js
@@ -1,39 +1,4 @@
 (function (ng) {
-    // Definición del módulo
-    var mod = ng.module("bookModule", ['ui.router']);
- 
-   // Configuración de los estados del módulo
-    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            // En basePath se encuentran los templates y controladores de módulo
-            var basePath = 'src/modules/books/';
-            // Mostrar la lista de libros será el estado por defecto del módulo
-            $urlRouterProvider.otherwise("/booksList");
-            // Definición del estado 'booksList' donde se listan los libros
-            $stateProvider.state('booksList', {
-                // Url que aparecerá en el browser
-                url: '/books/list',
-                // Se define una variable books (del estado) que toma por valor 
-                // la colección de libros que obtiene utilizando $http.get 
-                 resolve: {
-                    books: ['$http', function ($http) {
-                            return $http.get('data/books.json'); // $http retorna un apromesa que aquí no se está manejando si viene con error.
-                        }]
-                },
-                // Template que se utilizara para ejecutar el estado
-                templateUrl: basePath + 'books.list.html',
-                // El controlador guarda en el scope en la variable booksRecords los datos que trajo el resolve
-                // booksRecords será visible en el template
-                controller: ['$scope', 'books', function ($scope, books) {
-                        $scope.booksRecords = books.data;
-                    }]              
-            });
-        }
-    ]);
-    
-    
-})(window.angular);
-
-(function (ng) {
      // Definición del módulo
     var mod = ng.module("bookModule", ['ui.router']);
  
